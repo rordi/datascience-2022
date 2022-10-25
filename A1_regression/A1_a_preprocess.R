7dfbf158433440f164d63609b292e32092842988
Dietrich Rordorf <drordorf@gmail.com>
2022-10-25 23:29:03 +0200

    load raw data into data frame

--- A1_regression/A1_a_preprocess.R
+++ A1_regression/A1_a_preprocess.R
@@ -12,3 +12,16 @@
 
 # allow for reproducible results
 set.seed(1)
+
+# install package dependencies
+#install.packages("...")
+
+# load packages
+#library("...")
+
+# unzip raw data
+unzip("./A1_regression/LCdata.csv.zip", exdir = "./A1_regression")
+
+# load raw data from csv into data frame
+df <- read.csv("./A1_regression/LCdata.csv", sep=";")
+attributes(df)
