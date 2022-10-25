6ccbd89e9c319eed7ff339e7d5aa71a4e4db7d5c
Dietrich Rordorf <drordorf@gmail.com>
2022-10-25 22:55:10 +0200

    add separate scripts for preprocessing steps

---
+++ A1_regression/A1_a_preprocess.R
@@ -0,0 +1,14 @@
+# ---------------------------------------------------------------------
+# Assignment 1 - REGRESSION - PREPROCESS
+# ---------------------------------------------------------------------
+#
+# Group A2
+# Dietrich Rordorf, Marco Lecci, Rizoanun Nasa, Sarah Castratori
+#
+# This script is used to preprocess the raw data from CSV to useable
+# state.
+#
+# =====================================================================
+
+# allow for reproducible results
+set.seed(1)
