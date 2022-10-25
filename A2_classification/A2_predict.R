0814b21db649cf84e6124bcf00a640afb3a341bc
Dietrich Rordorf <drordorf@gmail.com>
2022-10-25 22:32:47 +0200

    copy deliverable instructions from assignment pdf

--- A2_classification/A2_predict.R
+++ A2_classification/A2_predict.R
@@ -8,4 +8,18 @@
 # This script is the final prediction script that will be
 # handed in and that will be used by the professors to test
 # the model on real-world unseen data.
+#
+# ➔ DELIVERABLE
 # ---------------------------------------------------------------------
+#
+# Provide the professors with a separate R file where:
+#
+#   1. professors can specify the file name of the file that holds the new data
+#   2. the file (i.e., the new data) is loaded
+#   3. the new data is transformed in the same way as the published, historic
+#      data (this includes all transformations that we did before we trained & 
+#      tested our model, including the splitting of the data into input & target
+#      data).
+# 4. our best trained model is loaded (functions are explained in the assignments)
+# 5. our best trained model is evaluated with the new data
+#
