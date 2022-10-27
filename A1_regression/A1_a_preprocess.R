d5806704d05265e759acd69fe330095e23855d11
Dietrich Rordorf <drordorf@gmail.com>
2022-10-27 08:51:37 +0200

    check for sparsity (missing values) in attributes

--- A1_regression/A1_a_preprocess.R
+++ A1_regression/A1_a_preprocess.R
@@ -14,10 +14,10 @@
 set.seed(1)
 
 # install package dependencies
-#install.packages("...")
+install.packages("scales")
 
 # load packages
-#library("...")
+library("scales")
 
 # unzip raw data
 unzip("./A1_regression/LCdata.csv.zip", exdir = "./A1_regression")
@@ -30,7 +30,8 @@ str(df)
 
 # initial observations:
 # --> we have 72 variables and ~798K observations
-# --> some variables have many missing values (NA) or many 0 values (sparse variables)
+# --> existing versus new applicants: new applicatnts have many data attributes missing (0 or NA) --> we may drop some of these attributes
+# --> some variables have many missing values (NA) or many 0 values (sparse variables) --> some customers are existing customers of LC versus new customers that may not have many of these attributes
 # --> numeric variables have several orders of magnitute difference (we may need to scale some numeric features to 0-1 scale after cutting off outliers)
 # --> "id" and "member_id" may be removed as they do not seem to bear any meaning
 # --> "term" is coded as string such as " 36 months"
@@ -43,8 +44,11 @@ str(df)
 # --> "desc" and "title" may need some NLP treatment
 # --> "zip_code" may need to be translated into latitude / longitude to be used as more appropriate geographical indicator
 # --> "last_pymnt_d" and "next_pymnt_d" are coded as dates, we may need to compute the delta in months instead
+# --> some applications are "joint" applications (co-borrowers, see field "verified_status_joint"), can be interpreted in order "not verified" < "income source verified" < "verified by LC"
+# 
+# scaling: we may keep the minimum as 0 and use a percentage such as 99% to cut-off outliers. Outliers are replaced with the treshold value. For instance for income if
+# threshold is 100'000 USD we will replace all outliers >100'000 USD with 100'000 USD. Then we scale from 0-1 scale.
 
-
-
-
+# check for sparsity of attributes
+percent(colMeans(is.na(df)))
 
